48e6e5135fe7516e1515f01575811a43189395fb
auxilia <auxilia@localhost>
2018-05-21 11:44:38 +0200

    tabel coterory

---
+++ database/migrations/2018_05_21_093903_create_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string("cat_name", 255)->collation('utf8_unicode_ci');
+            $table->string("cat_type" , 255)->collation('utf8_unicode_ci');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
