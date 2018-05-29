355e24a8bda89079fe064909e752b24145239144
Seb <my@mail.com>
2018-05-29 23:06:15 +0200

    Pas de zoom pour smartphone

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, user-scalable=no"><!-- initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0, -->
     <meta name="description" content="STOCK">
 
     <title>Gestion de stock | @yield('title')</title>
@@ -13,12 +13,12 @@
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.16/sl-1.2.5/datatables.min.css"/>
-	
+
 	<!-- The style by default -->
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}" media="screen" title="no title">
 	<!-- Our custom style -->
     <link rel="stylesheet" href="{{ asset('css/stock.css') }}" media="screen" title="no title">
-    
+
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
