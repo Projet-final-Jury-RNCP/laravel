26f8c45d26000fedf755a69c011812cb710d5af2
nils <nilspotet@gmail.com>
2018-05-29 18:33:03 +0200

    css

--- resources/views/stock/supply/index.blade.php
+++ resources/views/stock/supply/index.blade.php
@@ -7,10 +7,12 @@
 @section('content')
 <div class="container">
 	<div class="card m-3" id="stockSupply">
-		<div class="card-header">Gestion des approvisionnements <button  data-target="#update_modal" data-toggle="modal" data-source="sup-edit" data-token="{{ csrf_token() }}" class="btn btn-primary float-right" type="button" id="supply_button">Enregistrer</button></div>
+		<div class="card-header">
+			Inventaire - remise des qté du stock 
+			<button  class="btn btn-primary float-right" type="button" id="supply_button" onclick="document.supply.submit()">Enregistrer</button>
+		</div>
 		<div class="card-body">
 			<form action="{{ url('stock/approvisionner_update') }}" id="supply" method="post" name="supply">
-			<input type="submit" value="submit">
 				{{ csrf_field() }} {{ method_field('PUT') }}
 			<table id="table" class="table table-striped table-bordered" style="width: 100%">
 				<thead>
@@ -27,9 +29,7 @@
 						<td class="responsive-td" responsive-field="Categorie">{{ $stockSupply->product->category->cat_name }}</td>
 						<td class="responsive-td" responsive-field="Produit">{{ $stockSupply->product->name }}</td>
 						<td class="responsive-td" responsive-field="Quantité initiale">{{ $stockSupply->quantity }}</td>
-						<td class="responsive-td text-center" responsive-field="Quantité actuelle">{{ $loop->iteration }}
-<!-- 							<input type="number" name="qte_{{ $loop->iteration }}" value="{{ $stockSupply->quantity }}"> -->
-<!-- 							<input type="hidden" name="index_{{ $loop->iteration }}" value="{{ $stockSupply->id_product }}"> -->
+						<td class="responsive-td text-center" responsive-field="Quantité actuelle">
 							<input type="number" name="qte[{{ $stockSupply->id_product }}]" value="{{ $stockSupply->quantity }}">
 						</td>
 					</tr>
