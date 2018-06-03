688131f3fa08d0b92747f182f39942143bdfb1c2
nils <nilspotet@gmail.com>
2018-06-03 19:12:37 +0200

    correction filtre categorie dans le controleur produits

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -29,7 +29,7 @@ class ProductController extends Controller
     public function create()
     {
         $products = Product::all()->sortBy('name', SORT_NATURAL|SORT_FLAG_CASE);
-        $categories = Category::all()->sortBy('cat_name', SORT_NATURAL|SORT_FLAG_CASE);
+        $categories = Category::all()->sortBy('cat_name', SORT_NATURAL|SORT_FLAG_CASE)->where('active', true);
         $measures = MeasureUnit::all()->sortBy('measure_name', SORT_NATURAL|SORT_FLAG_CASE);
         return view ( 'stock.products.create', compact ( 'products', 'categories', 'measures' ) );
     }
