9f0e000db0703884b829adb1c1a387bb29d29671
nils <nilspotet@gmail.com>
2018-06-04 22:36:45 +0200

    rédaction documentation (aide)

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -37,7 +37,7 @@
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{  url('/stock/documentation') }}">Documentation</a>
+        <a class="nav-link" href="{{  url('/stock/documentation') }}">Aide</a>
       </li>
 <!--       <li class="nav-item"> -->
 <!--         <a class="nav-link" href="{{  url('/repas') }}">Repas</a> -->
