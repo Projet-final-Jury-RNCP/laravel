3ea14dcef9cf2e9bb017227c36ca68406de1f1c1
nils <nilspotet@gmail.com>
2018-06-06 16:30:53 +0200

    HTML sémantique

--- resources/views/stock/documentation/index.blade.php
+++ resources/views/stock/documentation/index.blade.php
@@ -5,25 +5,25 @@
 @stop
 
 @section('content')
-<div class="container documentation">
+<main class="container documentation">
 	<!-- The documentation -->
-	<div class="row">
+	<article class="row">
 		<h1>Aide</h1>
       	<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
       		<div class="row">
-      			<div class="documentation-section cm-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+      			<section class="documentation-section cm-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
       				<h2>Gestion des utilisateurs</h2>
       				<p>L’onglet « Utilisateurs » permet la création d’un nouvel utilisateur et l’affichage de la liste des utilisateurs avec pour chacun la possibilité de le modifier ou de le supprimer.<br />
       				La suppression d’un utilisateur ne détruit pas son compte mais le rend simplement inactif.<br />
       					L’utilisateur peut être réactivé de la sorte :
-      					<ul>
-      						<li>Cliquer sur « modifier »</li>
-      						<li>Choisir un mot de passe</li>
-      						<li>Enregistrer</li>
-      					</ul>
       				</p>
-      			</div>
-      			<div class="documentation-section cm-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+  					<ul>
+  						<li>Cliquer sur « modifier »</li>
+  						<li>Choisir un mot de passe</li>
+  						<li>Enregistrer</li>
+  					</ul>
+      			</section>
+      			<section class="documentation-section cm-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
       				<h2>Configuration des produits</h2>
       				<h3>Gestion des types de semaines</h3>
       				<p>
@@ -50,23 +50,22 @@
       					Lorsqu’un produit est créé ou modifié dans un type de semaine, la caractéristique « Quantité maximale » prend la valeur définie pour ce type de semaine et prend la valeur 0 pour tous les autres type de semaine.<br />
       					La suppression d’un produit dans un type de semaine supprime le produit de tous les autres types de semaine.
       				</p>
-      			</div>
-      			<div class="documentation-section col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+      			</section>
+      			<section class="documentation-section col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
       				<h2>Gestion du stock</h2>
-      				<p>Liste des actions possibles dans la gestion du stock :
-      					<ul>
-      						<li><b>Approvisionner</b> : permet l’entrée en stock d’une nouvelle acquisition. (Ex. enregistrement de produits achetés lors du retour de courses), en renseignant pour chaque produit la quantité et le prix unitaire.<br />
-      							Le prix unitaire proposé par défaut est celui qui a été renseigné dans la rubrique « Produits » du menu déroulant. Il est possible de modifier ce prix au moment de l'entrée en stock du produit.<br />
-      							<small><em>ATTENTION : Cette modification aura pour effet de modifier le prix unitaire défini dans la rubrique « Produits » du menu déroulant.</em></small>
-      							</li>
-      						<li><b>Inventorier</b> : permet de prendre connaissance de l’état du stock et pouvoir le remettre en état. (Ex. apporter un correctif suite à un inventaire).</li>
-      						<li><b>Sortie de stock</b> : lorsqu’un produit est consommé, enregistre la sortie de ce produit.</li>
-      						<li><b>Retour en stock</b> : permet de remettre en stock un produit finalement non utilisé. (Ex. 10 bouteilles de jus de fruits ont été sorties du stock, 3 n’ont finalement pas été bues, elle peuvent ainsi être remises en stock).<br />
-      						<small><em>ATTENTION : cette fonction ne doit pas être utilisée en cas d’achat d’un nouveau produit.</em></small></li>
-      					</ul>
-      				</p>
-      			</div>
-				<div class="documentation-section col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+      				<p>Liste des actions possibles dans la gestion du stock :</p>
+  					<ul>
+  						<li><b>Approvisionner</b> : permet l’entrée en stock d’une nouvelle acquisition. (Ex. enregistrement de produits achetés lors du retour de courses), en renseignant pour chaque produit la quantité et le prix unitaire.<br />
+  							Le prix unitaire proposé par défaut est celui qui a été renseigné dans la rubrique « Produits » du menu déroulant. Il est possible de modifier ce prix au moment de l'entrée en stock du produit.<br />
+  							<small><em>ATTENTION : Cette modification aura pour effet de modifier le prix unitaire défini dans la rubrique « Produits » du menu déroulant.</em></small>
+  							</li>
+  						<li><b>Inventorier</b> : permet de prendre connaissance de l’état du stock et pouvoir le remettre en état. (Ex. apporter un correctif suite à un inventaire).</li>
+  						<li><b>Sortie de stock</b> : lorsqu’un produit est consommé, enregistre la sortie de ce produit.</li>
+  						<li><b>Retour en stock</b> : permet de remettre en stock un produit finalement non utilisé. (Ex. 10 bouteilles de jus de fruits ont été sorties du stock, 3 n’ont finalement pas été bues, elle peuvent ainsi être remises en stock).<br />
+  						<small><em>ATTENTION : cette fonction ne doit pas être utilisée en cas d’achat d’un nouveau produit.</em></small></li>
+  					</ul>
+      			</section>
+				<section class="documentation-section col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
       				<h2>Faire les courses</h2>
       				<p>L’onglet « Faire les courses » dans le menu « Stock » permet d’afficher les produits qui sont actifs et dont la quantité en stock est inférieure à la quantité maximale définie. La colonne « quantité actuelle en stock » indique la quantité en stock ainsi que, entre parenthèses, les quantités minimale et maximale définies dans la fiche du produit. Les 2 colonnes de droite donnent les quantités à acheter pour atteindre le stock minimal ou maximal.
 						Un click sur la ligne d’un produit permet de signifier que ce produit a été ajouté dans le chariot de course ou dans le panier s’il s’agit de courses en ligne. Le bouton « Voir tout / Ce qui manque » permet d’afficher l’ensemble des produits à acheter ou seulement ceux qui n’ont pas déjà été ajoutés.
@@ -74,16 +73,16 @@
 					<p>	
 						En haut de cette page sont mentionnés le montant des courses en cours d'achat en &euro; ainsi que le montant maximal si tous les produits de la liste étaient achetés.
 					</p>
-      			</div>
-				<div class="documentation-section col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+      			</section>
+				<section class="documentation-section col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
       				<h2>Historique </h2>
       				<p>
       					La rubrique <b>Historique</b> permet de visualiser l'intégralité des entrées et sorties en stock.<br />
       					Le prix indiqué pour les approvisionnements est le prix unitaire du produit correspondant.
       				</p>
-      			</div>
+      			</section>
       		</div>
       	</div>
-    </div>
-</div>
+    </article>
+</main>
 @stop
