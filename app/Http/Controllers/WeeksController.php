777a767893e2349970eaf5ff8d6a55912e19d256
Seb <my@mail.com>
2018-06-06 19:53:52 +0200

    add products for new week

--- app/Http/Controllers/WeeksController.php
+++ app/Http/Controllers/WeeksController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 use App\Week;
+use App\WeekProduct;
 
 class WeeksController extends Controller
 {
@@ -17,7 +19,7 @@ class WeeksController extends Controller
 		$weeks = Week::all();
 		return view ( 'stock.weeks.create', compact ( 'weeks' ) );
 	}
-	
+
 	/**
 	 * Store a newly created resource in storage.
 	 *
@@ -29,16 +31,28 @@ class WeeksController extends Controller
 		$this->validate ( $request, [
 				'name' => 'required|min:5|max:255',
 		] );
-		
+
 		$week = new Week();
 		$week->name = $request->name;
 		$week->save();
-		
+
+		// Add for all weeks
+		$products = Product::all();
+		foreach ($products as $product) {
+
+		    $wp = new WeekProduct();
+		    $wp->id_week = $week->id;
+		    $wp->id_product = $product->id;
+		    $wp->max_threshold = 0;
+		    $wp->save();
+
+		}
+
 		\Session::flash('flash_message_success', 'Semaine créé : ' . $week->name);
-		
+
 		return redirect('stock/semaines');
 	}
-	
+
 	/**
 	 * Update the specified resource in storage.
 	 *
@@ -51,13 +65,13 @@ class WeeksController extends Controller
 		$this->validate ( $request, [
 				'name' => 'required|min:5|max:255',
 		] );
-		
+
 		$week = Week::find($request->index);
 		$week->name = $request->name;
 		$week->save();
-		
+
 		\Session::flash('flash_message_success','Semaine modifiée');
 		return redirect('stock/semaines');
 	}
-	
+
 }
